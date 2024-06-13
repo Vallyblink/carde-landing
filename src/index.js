e07aa0c0cd7a305f89d78d4621efcf93e5f470f0
Vallyblink <115736687+Vallyblink@users.noreply.github.com>
2024-06-13 17:44:35 +0300

    WORK!

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import {BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-   <Router basename="/carde-landing">
+   <Router>
       <App />
     </Router>
 );
